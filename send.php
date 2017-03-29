00fc6250d4d13869b5ec88c872a5a5e5de288b57
Aleksandr Vrublevskyi <al.vrublevskyi@gmail.com>
2017-03-30 01:07:14 +0300

    2.0 commit

--- send.php
+++ send.php
@@ -1,15 +1,17 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+if((isset($_GET['name'])&&$_GET['name']!="")&&(isset($_GET['phone'])&&$_GET['phone']!="")){ //Проверка отправилось ли наши поля name и не пустые ли они
         $to = 'al.vrublevskyi@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
-        $subject = 'Записаться на семинар'; //Загаловок сообщения
+        $subject = 'Записаться на семинар'; //Заголовок сообщения
         $message = '
         <html>
         <head>
             <title>'.$subject.'</title>
         </head>
         <body>
-            <p>Имя: '.$_POST['name'].'</p>
-            <p>Телефон: '.$_POST['phone'].'</p>                        
+            <p>Имя: '.$_GET['name'].'</p>
+            <p>Телефон: '.$_GET['phone'].'</p>
+            <p>Email: '.$_GET['email'].'</p>  
+            <p>День рождения: '.$_GET['birthday'].'</p>                          
         </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
